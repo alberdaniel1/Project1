6259890c9d5768f31eb53587cbd4e945217c107a
August Tornea <august.tornea@gmail.com>
2023-04-20 14:50:32 -0400

    finished working on the left list of user entries from localStorage

--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -1,87 +1,81 @@
-// august starts here
-//grab textbox user entry
 var API = "5VkpQWeFYgp2DIB7mOyEgg==WvtYDpjrBR0rVt5X";
-
 var displayRecipeCards = document.getElementById("displayRecipeCard");
 var letsEat = document.getElementById("letsEat");
+//window.onload =  saveAndDisplayLocalStorage;
+var dishEntryBoxEl = document.getElementById("dishEntryBox");
+var recipeListDisplayEl = document.getElementById('recipeListDisplay')
+
+let recipeItemsArray = localStorage.getItem('items') ?
+JSON.parse(localStorage.getItem('items')) : [];
+recipeItemsArray.forEach(addTask);
+function addTask(text){
+    const li = document.createElement('li')
+    li.textContent = text;
+    recipeListDisplayEl.appendChild(li);
+    li.addEventListener("click", function () {
+        
+    });
+}
+
+//Add an event listener to the list item
+
+
 
 letsEat.addEventListener("click", function () {
-    var textboxEl = document.getElementById("dishEntryBox").value;
-    console.log(textboxEl)
-    
-    
-    // adding the user's entered text from the texbox to localStorage
-    // console.log(localStorage)
-    var query = textboxEl;
+    var query = dishEntryBoxEl.value;
+    // localStorage.setItem("recipes", textboxEl.value);
+    // Call the API to get recipe results
     $.ajax({
         method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/recipe?query=' + query,
+        url: 'https://api.api-ninjas.com/v1/recipe',
         headers: { 'X-Api-Key': API },
-        contentType: 'application/json',
+        data: { query: query },
         success: function (result) {
             displayRecipeCards.innerHTML = "";
-            console.log(result);
-            // for loop to display on Recipe Card
-            for(var i = 0; i < 4; i++) {
-                var recipeTitle = result[i].title;
-                var recipeIngredients = result[i].ingredients;
-                var display50chars = recipeIngredients.substring(0,50);
+            for (var i = 0; i < 4; i++) {
+                var recipe = result[i];
                 var listTitle = document.createElement('li');
-                listTitle.setAttribute("id" , "title")
-                var listIngredients = document.createElement('li');
-                listIngredients.setAttribute("id" , "ingredients");
-                listTitle.innerHTML = recipeTitle;
-                listIngredients.innerHTML = display50chars;
-                
+                listTitle.setAttribute("id", "title")
+                listTitle.innerHTML = recipe.title;
                 displayRecipeCards.appendChild(listTitle);
-                displayRecipeCards.appendChild(listIngredients);
-                console.log(result[i].title);
-                
+                if (recipe.ingredients) {
+                    var listIngredients = document.createElement('li');
+                    listIngredients.setAttribute("id", "ingredients");
+                    listIngredients.innerHTML = recipe.ingredients.substring(0, 50);
+                    displayRecipeCards.appendChild(listIngredients);
+                }
             }
-            
         },
-        error: function ajaxError(jqXHR) {
+        error: function (jqXHR) {
             console.error('Error: ', jqXHR.responseText);
-            
-            fetchButton.addEventListener('click');  
         }
-    }); 
-      
     });
-    function displaySavedRecipes() {
-        // Get the saved recipes from localStorage
-        var savedRecipes = localStorage.getItem("recipes");
-      
-        // If there are saved recipes, display them in a list
-        if (savedRecipes) {
-          // Split the saved recipes into an array
-          var recipesArray = savedRecipes.split(",");
-      
-          // Get the container element to display the list
-          var recipeList = document.getElementById("recipeList");
-      
-          // Loop through the recipes array and create a new list item for each recipe
-          for (var i = 0; i < recipesArray.length; i++) {
-            var recipeListItem = document.createElement("li");
-            recipeListItem.innerText = recipesArray[i];
-            recipeListItem.style.borderBottom = "1px solid #ccc";
-            recipeListItem.style.backgroundColor = "#f9f9f9";
-      
-            // Add an event listener to the list item
-            recipeListItem.addEventListener("click", function() {
-              console.log(this.innerText);
-            });
-      
-            recipeList.appendChild(recipeListItem);
-          }
-        }
-      }
-      
-      // Call the function to display saved recipes on page load
-      
-      
-      window.onload = displaySavedRecipes;
-      
+    recipeItemsArray.push(dishEntryBoxEl.value);
+    localStorage.setItem('items', JSON.stringify(recipeItemsArray));
+    addTask(dishEntryBoxEl.value);
+    dishEntryBoxEl.value = '';
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Luc's work here
 
@@ -113,22 +107,22 @@ recipeCard.addEventListener("click", function () {
 
 
 function searchYouTube(recipe, key) {
-    var videoSearch = "https://www.googleapis.com/youtube/v3/search?key=" +key+ "&type=video&part=snippet&maxResults=1&q=" + recipe;
+    var videoSearch = "https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=1&q=" + recipe;
 
     fetch(videoSearch, {})
-    .then(function (response){
-        if(response.status !== 200){
-            videoTitle.textContent = "Youtube is currently experiencing issues please try again later";
+        .then(function (response) {
+            if (response.status !== 200) {
+                videoTitle.textContent = "Youtube is currently experiencing issues please try again later";
+                return;
+            } else {
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            videoTitle.textContent = data.items[0].snippet.title;
+            videoDisplay.src = youtubeURL + data.items[0].id.videoId;
+            videoDisplay.width = "100%";
+            videoDisplay.height = "80%";
             return;
-        }else{
-        return response.json();
-        }
-    })
-    .then(function (data){
-        videoTitle.textContent = data.items[0].snippet.title;
-        videoDisplay.src = youtubeURL + data.items[0].id.videoId;
-        videoDisplay.width = "100%";
-        videoDisplay.height = "80%";
-        return;
-    })
+        })
 }
